1d3398eda84e340cafc9dc1c836acea768c5129c
ankur <ankur.tyagi4@cognizant.com>
2026-04-20 21:28:59 +0530

    Fixing naming conventions for api

--- ClinicFlow-Backend/Controllers/ClinicsController.cs
+++ ClinicFlow-Backend/Controllers/ClinicsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicFlow_Backend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ClinicsController : ControllerBase
     {
